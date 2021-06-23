643ac09db6009bf9904248ef474372661fe83653
Eszter Legar <eszter@streambrightdata.com>
2021-06-23 14:30:02 +0200

    next step

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import ReverseEngineeringTask from "./ReverseEngineeringTask";
 
 function App() {
+  const [state, setState] = useState<string |null>(null);
+
   return (
     <div className="App">
-      <ReverseEngineeringTask />
+      <div className="Header">Fruit Selector</div>
+      <div className="Text">Selected: {state}</div>
+      <div className="List">
+        <div className="Item" onClick={()=> setState('Apple')}>🍎</div>
+        <div className="Item" onClick={()=> setState('Orange')}>🍊</div>
+        <div className="Item" onClick={()=> setState('Banana')}>🍌</div>
+      </div>
     </div>
   );
 }
